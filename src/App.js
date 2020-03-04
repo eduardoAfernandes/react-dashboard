74a4eb0b0cd17921ec1a3435f3fbf764dae251ae
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-03-04 20:44:37 -0300

    Exibindo menu superior

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Importando componente Menu Superior para renderiza-lo na pagina
+import MenuSuperior from './Components/MenuSuperior/MenuSuperior';
 
-      
-    </div>
+function App() {
+    return (
+     <div>
+       <MenuSuperior/>
+     </div>
   );
+
 }
 
 export default App;
---
+++ src/Components/MenuSuperior/MenuSuperior.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class MenuSuperior extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <a className="navbar-brand" href="#">Dashboard</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(Página atual)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Destaques</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Preços</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">Desativado</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default MenuSuperior;
